eb646da8a4e230bc0550abf7e2a938ed1d909e54
nicokrafterra <gamerjuan442@gmail.com>
2023-10-02 17:59:39 -0500

    proy_02 JNOM

---
+++ app.js
@@ -0,0 +1,10 @@
+require ("colors")
+const functions = require ('./functions.js')
+
+console.log(functions);
+process.stdout.write("Ejercicio\n\n")
+
+console.log(functions.add(5,3));
+console.log(functions.substract(2,8));
+console.log(functions.multiply(2,8));
+console.log(functions.divide(2,0.9));
